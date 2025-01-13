49e5de9ff28d3398b73b7223591fd061142328fd
ChefTandy17 <ttandy@ucsc.edu>
2025-01-13 13:23:45 -0800

    figure out error. config object suppose to be first.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-let game = new Phaser.Game(config)
-
 let config = {
-    type: Phaser.AUTO,
-    width: 640,
-    height: 480
-  }
+  type: Phaser.AUTO,
+  width: 640,
+  height: 480
+}
 
+let game = new Phaser.Game(config)
 
