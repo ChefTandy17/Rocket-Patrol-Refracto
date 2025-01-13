2daa2fac6c0ef05dd9cb27ed584ef10de36479c8
ChefTandy17 <ttandy@ucsc.edu>
2025-01-13 14:28:18 -0800

    testing

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,7 +7,8 @@ class Play extends Phaser.Scene{
         //this.add.text(20, 20, "Rocket Patrol Play");
         
         // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2,
+        0x00FF00).setOrigin(0, 0);
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
