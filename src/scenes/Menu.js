ead1b89c31fb75712d5ec902f594944867eb528d
ChefTandy17 <ttandy@ucsc.edu>
2025-01-13 16:58:09 -0800

    added assets (will github work right now)

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,6 +3,15 @@ class Menu extends Phaser.Scene{
         super("menuScene");
     }
 
+    preload(){
+        //load images/tile sprites
+        //load.images(string of the graphic, url of the graphic is located)
+        this.load.image('rocket', './assets/rocket.png')
+        this.load.image('spaceship', './assets/spaceship.png')
+        this.load.image('starfield', './assets/starfield.png')
+    }
+
+
     create(){
         this.add.text(20, 20, "Rocket Patrol Menu") 
         this.scene.start("playScene");
